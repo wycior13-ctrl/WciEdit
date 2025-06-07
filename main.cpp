b8980658e633e8b901fa328e4191eee7de29e155
wycior13 <wycior13@users.noreply.github.com>
2025-06-07 16:13:08 +0200

    add the comments

--- main.cpp
+++ main.cpp
@@ -6,21 +6,25 @@
 #include <sstream>
 
 int main() {
-     using namespace std;
-     string fname;
-     stringstream text;
+     using namespace std; 
+     string fname; //the filename string
+     stringstream text; //the text string
 
      cout << "please give the filename: ";
      cin >> fname;
-     ifstream workfile;
-     workfile.open(fname);
+     ifstream workfile; //make the workfile object
+     workfile.open(fname);// open the file
 
-     if (!workfile.is_open())
+     if (!workfile.is_open()) //look if the file is ok
      {
-          exit(EXIT_FAILURE);
+          exit(EXIT_FAILURE);//exit if not
      }
 
-     text << workfile.rdbuf();
-     cout << endl << text.str() << endl;
+     text << workfile.rdbuf(); //add the filetext to the text string
+     cout << endl << text.str() << endl;//write the textstring
 
-     return 0;}
+     return 0;} //end of code
+
+     //Developers:
+     //wycior13\wycior13-ctrl
+     //no more
